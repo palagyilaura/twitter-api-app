f70e3beb8720234484190598b84288b23c423d42
LaraP <palagyilaura@gmail.com>
2021-08-25 15:05:43 +0200

    App fetch data from twitter API

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const request = require('request');
+const port = 5000; //react-tel ne ugyanazon a porton legyen!
+
+app.get("/", (req, res) => {
+    res.send("Hello world!")
+});
+
+const options = {
+    'method': 'GET',
+    'url': 'https://api.twitter.com/1.1/search/tweets.json?q=%23UX&count=10&tweet_mode=extended',
+    'headers': {
+        'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAF7OMwEAAAAAkP9G4gBKE%2B7%2FDbIXcKnipeeVsCU%3Da5AsW9DkaIbrto0JnOSPLbYHU145vgDTx5BsRsp1OjdbHpAjU7',
+        'Cookie': 'guest_id=v1%3A161390876970956770; personalization_id="v1_ZD0ZiDvLLzQTGeyaMr+rpA=="; lang=hu'
+    }
+};
+
+app.get("/getTweets", (req, res) => {
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        //console.log(response.body);
+
+        let parseBody = JSON.parse(body)
+        let tweets = parseBody["statuses"];
+        let tweetList = [];
+
+        for (let tweet in tweets) {
+            //console.log(tweet, ":", tweets[tweet]["text"]);
+
+            tweetList.push({ name: tweets[tweet]["user"]["name"], text: tweets[tweet]["full_text"] });
+        }
+
+        res.send(tweetList);
+        console.log(tweetList);
+    });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
